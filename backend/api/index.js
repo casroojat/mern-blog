d32f91ceae73fa79f6ab522242c1ebfdc2e3f47e
Casro Ojat <casro.ojat@gmail.com>
2024-01-23 08:42:36 +0700

    create a tes API route

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/User.route.js'
 
 dotenv.config();
 
@@ -18,3 +19,5 @@ const port = 3000;
 app.listen(port, ()=>{
     console.log('Server running on port 3000');
 });
+
+app.use('/api/user', userRoutes);
